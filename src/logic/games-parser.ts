1f9004e22d0e55d4b375eafac0490b1c95264028
PhilSchoe <p.schoemacker@gmx.de>
2024-05-09 19:49:45 +0200

    Begin parsing series

--- src/logic/games-parser.ts
+++ src/logic/games-parser.ts
@@ -5,7 +5,19 @@ export function parseGames(gamesJson: []): void {
     return;
   }
 
+  const series = new Map<string, Series>();
+
   gamesJson.forEach((game: Game) => {
     console.log(game);
+
+    const homeTeam = game.home_team;
+    const visitorTeam = game.visitor_team;
+
+    const seriesKey = homeTeam.abbreviation + visitorTeam.abbreviation;
+    if (series.has(seriesKey)) {
+    } else {
+    }
   });
 }
+
+function createSeries(games: Game): Series {}
