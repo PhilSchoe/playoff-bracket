dc2bd4c21f640bef60a056c26d10372fea295fee
PhilSchoe <p.schoemacker@gmx.de>
2024-05-23 17:20:28 +0200

    Add sass and little styling for playoff-series
    - Add scss file for playoff-series
    - Play around with styling of playoff-series component

--- src/components/playoff-series.tsx
+++ src/components/playoff-series.tsx
@@ -1,13 +1,15 @@
+import styles from "./playoff-series.module.scss";
 import Series from "../types/series";
 
 export default function PlayoffSeries({ series }: { series: Series }) {
   return (
-    <div>
-      <div>{`${series.getFirstTeam().name} ${series.getFirstTeamWins()}`}</div>
-      <div>{`${
+    <div className={styles["playoff-series"]}>
+      <div className={styles["playoff-series__first-team"]}>{`${
+        series.getFirstTeam().name
+      } ${series.getFirstTeamWins()}`}</div>
+      <div className={styles["playoff-series__second-team"]}>{`${
         series.getSecondTeam().name
       } ${series.getSecondTeamWins()}`}</div>
-      <br></br>
     </div>
   );
 }
